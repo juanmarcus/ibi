31581e223ae6ce51493118b8433a98f7c774cab2
Juan Ibiapina <juanmarcus@gmail.com>
2009-10-13 17:11:54 -0300

    Create an action for loading images in ImageWidget. Not tested.

--- src/ibi_qt/ImageWidget.cpp
+++ src/ibi_qt/ImageWidget.cpp
@@ -1,11 +1,43 @@
 #include "ImageWidget.h"
 
+#include <QtGui/QLayout>
+#include <QtGui/QLabel>
+#include <QtGui/QFileDialog>
+
 ImageWidget::ImageWidget(QWidget *parent) :
-	QGLWidget(parent)
+	QWidget(parent)
 {
+	QVBoxLayout* layout = new QVBoxLayout();
+	this->setLayout(layout);
 }
 
 ImageWidget::~ImageWidget()
 {
 
+}
+
+void ImageWidget::addImage(QString filename)
+{
+	QPixmap pixmap(filename);
+	this->addImage(pixmap);
+}
+
+void ImageWidget::addImage(QPixmap pixmap)
+{
+	if (!pixmap.isNull())
+	{
+		QLabel* label = new QLabel();
+		label->setPixmap(pixmap);
+		this->layout()->addWidget(label);
+	}
+	//TODO save pixmap for further reorganization
+}
+
+void ImageWidget::addImage()
+{
+	QString filename = QFileDialog::getOpenFileName(this, "Select image");
+	if (!filename.isEmpty())
+	{
+		this->addImage(filename);
+	}
 }
--- src/ibi_qt/ImageWidget.h
+++ src/ibi_qt/ImageWidget.h
@@ -1,15 +1,20 @@
 #ifndef IMAGEWIDGET_H
 #define IMAGEWIDGET_H
 
-#include <QtOpenGL/QGLWidget>
+#include <QtGui/QWidget>
 
-class ImageWidget: public QGLWidget
+class ImageWidget: public QWidget
 {
 Q_OBJECT
 
 public:
 	ImageWidget(QWidget *parent = 0);
 	~ImageWidget();
+
+public slots:
+	void addImage();
+	void addImage(QPixmap pixmap);
+	void addImage(QString filename);
 
 };
 
