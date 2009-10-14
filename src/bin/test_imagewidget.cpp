31d1c8117885140311628d32085369b501f4642f
Juan Ibiapina <juanmarcus@gmail.com>
2009-10-14 10:02:17 -0300

    Add action to add images to the ImageWidget.
    Tested image adding.

--- src/bin/test_imagewidget.cpp
+++ src/bin/test_imagewidget.cpp
@@ -7,6 +7,9 @@
 
 #include <QtGui/QApplication>
 #include <QtGui/QMainWindow>
+#include <QtGui/QAction>
+#include <QtGui/QToolBar>
+#include <QtCore>
 
 #include "ibi_qt/ImageWidget.h"
 
@@ -16,9 +19,19 @@ int main(int argc, char **argv)
 	QApplication app(argc, argv);
 	// Create a window
 	QMainWindow w;
-	// Set the central widget to a Imagewidget
+
+	// Set the central widget to a ImageWidget
 	ImageWidget* imageWidget = new ImageWidget();
 	w.setCentralWidget(imageWidget);
+
+	// Configure actions
+	QAction* action = new QAction(w.tr("Add Image"), &w);
+	action->connect(action, SIGNAL(triggered()), imageWidget, SLOT(addImage()) );
+
+	// Configure toolbar
+	QToolBar* bar = w.addToolBar(w.tr("Actions"));
+	bar->addAction(action);
+
 	// Show the window
 	w.show();
 	// Run the application loop
