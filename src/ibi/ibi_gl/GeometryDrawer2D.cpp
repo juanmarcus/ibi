d21be6a2e88b428cebe21010afd8686c12331a4a
Juan Ibiapina <juanmarcus@gmail.com>
2009-11-11 11:16:56 -0200

    Add GeometryDrawer2D.

---
+++ src/ibi/ibi_gl/GeometryDrawer2D.cpp
@@ -0,0 +1,39 @@
+/*
+ * GeometryDrawer2D.cpp
+ *
+ *  Created on: Nov 11, 2009
+ *      Author: Juan Ibiapina
+ */
+
+#include "GeometryDrawer2D.h"
+
+namespace ibi
+{
+
+GeometryDrawer2D::GeometryDrawer2D()
+{
+
+}
+
+GeometryDrawer2D::~GeometryDrawer2D()
+{
+
+}
+
+void GeometryDrawer2D::drawCircle(Vector3& center, float radius,
+		float nbSubdivisions)
+{
+	float step = 360.0 / nbSubdivisions;
+	glBegin(GL_TRIANGLE_FAN);
+
+	glVertex2f(center.x, center.y);
+	for (float angle = 0.0; angle <= 360; angle += step)
+	{
+		glVertex2f(center.x + Math::Sin(Degree(angle)) * radius, center.y
+				+ Math::Cos(Degree(angle)) * radius);
+	}
+
+	glEnd();
+}
+
+} // namespace ibi
---
+++ src/ibi/ibi_gl/GeometryDrawer2D.h
@@ -0,0 +1,30 @@
+/*
+ * GeometryDrawer2D.h
+ *
+ *  Created on: Nov 11, 2009
+ *      Author: Juan Ibiapina
+ */
+
+#ifndef GEOMETRYDRAWER2D_H_
+#define GEOMETRYDRAWER2D_H_
+
+#include "ibi.h"
+#include "ibi_gl.h"
+#include "ibi_geometry/Vector3.h"
+
+namespace ibi
+{
+
+class GeometryDrawer2D
+{
+public:
+	GeometryDrawer2D();
+	~GeometryDrawer2D();
+
+	void drawCircle(Vector3& center, float radius = 10.0, float nbSubdivisions =
+			60);
+};
+
+} // namespace ibi
+
+#endif /* GEOMETRYDRAWER2D_H_ */
