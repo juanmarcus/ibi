3ea041c0c65e4311a3e22e3e356e4a2c180b285e
Juan Ibiapina <juanmarcus@gmail.com>
2009-11-24 16:08:52 -0200

    Add suport for 1D textures from QImages.

--- src/ibi/ibi_qt/TextureConfigurator_Qt.h
+++ src/ibi/ibi_qt/TextureConfigurator_Qt.h
@@ -24,11 +24,35 @@ public:
 
 		// Allocate and set memory
 		char* data = new char[t.numBytes()];
-		memcpy(data,t.bits(),t.numBytes());
+		memcpy(data, t.bits(), t.numBytes());
 
 		info.internalformat = GL_RGBA;
 		info.width = t.width();
 		info.height = t.height();
+		info.format = GL_RGBA;
+		info.type = GL_UNSIGNED_BYTE;
+		info.data = data;
+		info.destroydata = true;
+
+		return info;
+	}
+
+	static inline TextureLoadingInfo fromQImage1D(QImage &image)
+	{
+		// Create the resulting object
+		TextureLoadingInfo info;
+
+		// Convert the image to OpenGL format
+		QImage t = QGLWidget::convertToGLFormat(image);
+
+		// Allocate and set memory
+		char* data = new char[t.bytesPerLine()];
+		memcpy(data, t.bits(), t.bytesPerLine());
+
+		info.target = GL_TEXTURE_1D;
+		info.dim = 1;
+		info.internalformat = GL_RGBA;
+		info.width = t.width();
 		info.format = GL_RGBA;
 		info.type = GL_UNSIGNED_BYTE;
 		info.data = data;
